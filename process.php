575fcf457b6668bf40090b2e460fde0a7e445b70
nidula6 <nidulajayawardana@gmail.com>
2024-06-08 02:52:15 +0530

    fixed some errors

--- process.php
+++ process.php
@@ -10,24 +10,24 @@ $email = '';
 $username = '';
 
 
-if (isset($_POST['save'])) {
+// if (isset($_POST['save'])) {
 
-    print_r($_POST);
+//     print_r($_POST);
 
-    $user_id = $_POST['user_id'];
-    $firstname = $_POST['firstname'];
-    $email = $_POST['email'];
-    $lastname = $_POST['lastname'];
-    $username = $_POST['username'];
+//     $user_id = $_POST['user_id'];
+//     $firstname = $_POST['firstname'];
+//     $email = $_POST['email'];
+//     $lastname = $_POST['lastname'];
+//     $username = $_POST['username'];
 
-    $sql = "INSERT INTO user (user_id, first_name, last_name, username,  email) VALUES ('$user_id', $firstname, '$emlastnameail','$username','$email')";
+//     $sql = "INSERT INTO user (user_id, first_name, last_name, username,  email) VALUES ('$user_id', $firstname, '$emlastnameail','$username','$email')";
 
-    $conn->query($sql) or die($conn->error);
-    echo "Record has been saved!";
-    echo "Now you are in the process.php file $_POST[save]";
+//     $conn->query($sql) or die($conn->error);
+//     echo "Record has been saved!";
+//     echo "Now you are in the process.php file $_POST[save]";
 
-    header("Location: admin_page.php");
-}
+//     header("Location: admin_page.php");
+// }
 
 if (isset($_GET['delete'])) {
 
@@ -45,23 +45,54 @@ if (isset($_GET['delete'])) {
 }
 
  
-if (isset($_POST['edit'])) {
+// if (isset($_POST['edit'])) {
 
-    $user_id = $_POST['user_id'];
-    $email = $_POST['email'];
-   $firstname = $_POST['firstname'];
+//     $user_id = $_POST['user_id'];
+//     $email = $_POST['email'];
+//    $firstname = $_POST['firstname'];
    
-   $lastname = $_POST['lastname'];
-   $username = $_POST['username'];
+//    $lastname = $_POST['lastname'];
+//    $username = $_POST['username'];
 
-    $sql = "UPDATE user SET email='$email', first_name='$firstname' , last_name='$lastname' , username='$username' WHERE user_id = '$user_id'";
+//     $sql = "UPDATE user SET email='$email', first_name='$firstname' , last_name='$lastname' , username='$username' WHERE user_id = '$user_id'";
     
-    $conn->query($sql) or die($conn->error);
+//     $conn->query($sql) or die($conn->error);
 
  
-    $_SESSION['message'] = "Record has been Updated!";
-    $_SESSION['msg_type'] = "warning";
+//     $_SESSION['message'] = "Record has been Updated!";
+//     $_SESSION['msg_type'] = "warning";
+//     header("Location: admin_page.php");
+// }
+if (isset($_POST['edit'])) {
+    $user_id = $_POST['user_id'];
+    $email = $_POST['email'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $username = $_POST['username'];
+
+    // Check if the user ID already exists in the user table
+    $check_sql = "SELECT COUNT(*) as count FROM user WHERE user_id = '$user_id'";
+    $check_result = $conn->query($check_sql);
+    $check_row = $check_result->fetch_assoc();
+
+    if ($check_row['count'] > 0) {
+        // If the user ID exists, update the user data
+        $update_sql = "UPDATE user SET email='$email', first_name='$firstname', last_name='$lastname', username='$username' WHERE user_id = '$user_id'";
+        if ($conn->query($update_sql) === TRUE) {
+            $_SESSION['message'] = "Record has been Updated!";
+            $_SESSION['msg_type'] = "warning";
+        } else {
+            $_SESSION['message'] = "Error updating record: " . $conn->error;
+            $_SESSION['msg_type'] = "danger";
+        }
+    } else {
+        $_SESSION['message'] = "User ID does not exist!";
+        $_SESSION['msg_type'] = "danger";
+    }
+
+    // Redirect back to admin_page.php
     header("Location: admin_page.php");
+    exit();
 }
 
 ?>
